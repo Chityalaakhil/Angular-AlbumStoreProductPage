e9408f2ac60dc7fe88f4e10fa98ce7ec1b9846e5
Chityalaakhil <akhil.ch49@gmail.com>
2022-01-03 15:46:40 +0530

    Completed module 5

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,15 +3,21 @@ import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
 import { Album } from './album';
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
 
   _albumUrl = '../assets/album.json';
+  _productsUrl = './assets/products.json';
 
   constructor( private _http:Http ) { }
 
   getAlbum( id:number ): Observable<Album> {
     return this._http.get(this._albumUrl).map( res => <Album>res.json());
+  }
+
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map( res => <Product[]>res.json());
   }
 }
